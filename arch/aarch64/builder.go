ff1d1294c98d95c65877aad7ae873889205813a8
chenzhuoyu <chenzhuoyu1992@gmail.com>
2024-09-05 17:59:42 +0800

    fix: comments

--- arch/aarch64/builder.go
+++ arch/aarch64/builder.go
@@ -6,7 +6,7 @@ import (
     `github.com/chenzhuoyu/iasm/asm`
 )
 
-// Builder returns the x86_64 specific instruction builder.
+// Builder returns the AArch64 specific instruction builder.
 func Builder(prog *asm.Program) *Program {
     return (*Program)(unsafe.Pointer(prog))
 }
